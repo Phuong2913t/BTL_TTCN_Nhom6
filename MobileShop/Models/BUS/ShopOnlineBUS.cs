5251867a6dc3d4057048c0bc31d84c0cc22c9b10
ThuPhuong <nguyenthuphuong2913@gmail.com>
2023-04-12 16:12:13 +0700

    BaiTapLon da co trang Home, product, phan trang, chi tiet san pham, da load csdl

---
+++ MobileShop/Models/BUS/ShopOnlineBUS.cs
@@ -0,0 +1,22 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class ShopOnlineBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<SanPham>("Select * from SanPham where TinhTrang = 0");
+        }
+        public static SanPham Describe(String a)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.SingleOrDefault<SanPham>("Select * from SanPham where MaSanPham = @0", a);
+        }
+    }
+}
