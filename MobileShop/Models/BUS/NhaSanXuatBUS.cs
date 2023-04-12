bc75639a295a40de2ea4328f1fc2c902cf382381
Longg0223 <longg0223@gmail.com>
2023-04-12 18:49:44 +0700

    abcd

---
+++ MobileShop/Models/BUS/NhaSanXuatBUS.cs
@@ -0,0 +1,17 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MobileShop.Models.BUS
+{
+    public class NhaSanXuatBUS
+    {
+        public static IEnumerable<NhaSanXuat> DanhSach()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<NhaSanXuat>("Select * from NhaSanXuat where TinhTrang = 0");
+        }
+    }
+}
