4940bea98c3f14db6d6f8d8c62245b1b3d6a1227
Longg0223 <longg0223@gmail.com>
2023-05-06 20:06:58 +0700

    Da fix loi menu

--- MobileShop/Models/BUS/LoaiSanPhamBUS.cs
+++ MobileShop/Models/BUS/LoaiSanPhamBUS.cs
@@ -6,17 +6,17 @@ using System.Web;
 
 namespace MobileShop.Models.BUS
 {
-    public class NhaSanXuatBUS
+    public class LoaiSanPhamBUS
     {
-        public static IEnumerable<NhaSanXuat> DanhSach()
+        public static IEnumerable<LoaiSanPham> DanhSach()
         {
             var db = new ShopOnlineConnectionDB();
-            return db.Query<NhaSanXuat>("Select * from NhaSanXuat where TinhTrang = 0");
+            return db.Query<LoaiSanPham>("Select * from LoaiSanPham where TinhTrang = 0");
         }
         public static IEnumerable<SanPham> ChiTiet(String id)
         {
             var db = new ShopOnlineConnectionDB();
-            return db.Query<SanPham>("Select * from SanPham where MaNhaSanXuat = '" + id + "'");
+            return db.Query<SanPham>("Select * from SanPham where MaLoaiSanPham = '" + id + "'");
         }
     }
 }
